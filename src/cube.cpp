bb5960d551bfc72575d3af8891c4f8db3fd17bea
KristianHooymans <hooymanskristian@gmail.com>
2026-07-06 14:54:25 +1000

    calculating functions

--- src/cube.cpp
+++ src/cube.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 #include <cmath>
 
+double A, B, C;
+
 const int width = 80;
 const int height = 60;
 char video[width * height];
 double zbuffer[width * height];
+
+double cX(int x, int y, int z) {
+  return y * std::sin(A) * std::sin(B) * std::cos(C) - z * std::cos(A) * std::sin(B) * std::cos(C) + y * std::cos(A) * std::sin(C) + z * std::sin(A) * std::sin(C) + x * std::cos(B)* std::cos(C);
+} 
+
+double cY(int x, int y, int z) {
+  return y * std::cos(A) * std::cos(C) + z * std::sin(A) * std::cos(C) - y * std::sin(A) * std:sin(B) * std::sin(C) + z * std::cos(A) *std::sin(B) * std::sin(C) - x * std::cos(B) * std::sin(C);
+}
+
+double cZ(int x, int y, int z) {
+  
+}
 
 int main() {
 

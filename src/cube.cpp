b0d4eb9eeb6da183b4b74f860c05365977cdccce
KristianHooymans <hooymanskristian@gmail.com>
2026-07-06 10:22:50 +1000

    Restarting

--- src/cube.cpp
+++ src/cube.cpp
@@ -2,34 +2,10 @@
 #include <cmath>
 
 const int width = 80;
-const int height = 40;
-const int video[width * height];
-
-
-void normalise(double x, double y, double z) {
-  double norm = std::sqrt(x * x + y * y + z * z);
-  if (norm > 0.00000001) {
-    x /= norm;
-    y /= norm;
-    z /= norm;
-  }
-}
-
+const int height = 60;
+char video[width * height];
+double zbuffer[width * height];
 
 int main() {
-  double x, y, z;
-  std::cout << "Input x y z ";
-  std::cin >> x >> y >> z;
 
-  std::vector<double> rotationalAxis(2);
-  rotationalAxis[0] = x;
-  rotationalAxis[1] = y;
-  rotationalAxis[2] = z;
-
-  double angle = 0.0;
-  double diffAngle = 0.05;
-
-
-
-  return 0;
 }

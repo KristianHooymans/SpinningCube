bbf8d7a72452f7d55a18fe29561de2563dc03b21
KristianHooymans <hooymanskristian@gmail.com>
2026-07-07 12:24:15 +1000

    changing the look of the cub e

--- src/cube.cpp
+++ src/cube.cpp
@@ -14,19 +14,23 @@ const int height = 40;
 char video[width * height];
 double zbuffer[width * height];
 
-double cX(double x, double y, double z) {
+double cX(double x, double y, double z) //calculates the X rotational coordinate
+{
   return y * std::sin(A) * std::sin(B) * std::cos(C) - z * std::cos(A) * std::sin(B) * std::cos(C) + y * std::cos(A) * std::sin(C) + z * std::sin(A) * std::sin(C) + x * std::cos(B)* std::cos(C);
 } 
 
-double cY(double x, double y, double z) {
+double cY(double x, double y, double z) //calculates the Y rotational coordinate
+{
   return y * std::cos(A) * std::cos(C) + z * std::sin(A) * std::cos(C) - y * std::sin(A) * std::sin(B) * std::sin(C) + z * std::cos(A) *std::sin(B) * std::sin(C) - x * std::cos(B) * std::sin(C);
 }
 
-double cZ(double x, double y, double z) {
+double cZ(double x, double y, double z) // calculates the Z rotational coordinate
+{
   return z * std::cos(A) * std::cos(B) - y * std::sin(A) * std::cos(B) + x * std::sin(B);  
-}
+} 
 
-void plot(double fx, double fy, double fz, char ch) {
+void plot(double fx, double fy, double fz, char ch) //plots the 3D image on the 2D display
+{
   Vector3D p = {cX(fx, fy, fz), cY(fx, fy, fz), cZ(fx, fy, fz) };
   double depth = p.z + distFromCam;
   int screenX = width/2 + K1 * p.x / depth * 2;
@@ -49,15 +53,15 @@ int main() {
     for (double cx = -cubeWidth; cx < cubeWidth; cx += 0.15) {
       for (double cy = -cubeWidth; cy < cubeWidth; cy += 0.15) {
         plot(cx, cy, cubeWidth, '#');
-        plot(cx, cy, -cubeWidth, '.');
-        plot(cubeWidth, cy, cx, '?');
-        plot(-cubeWidth, cy, cx, '$');
-        plot(cx, cubeWidth, cy, '!');
-        plot(cx, -cubeWidth, cy, '@');
+        plot(cx, cy, -cubeWidth, '!');
+        plot(cubeWidth, cy, cx, '~');
+        plot(-cubeWidth, cy, cx, ';');
+        plot(cx, cubeWidth, cy, '$');
+        plot(cx, -cubeWidth, cy, '+');
       }
     }
   
-    for ( int row = 0; row < height; ++row) {
+    for (int row = 0; row < height; ++row) {
       for (int col = 0; col < width; ++col) {
         
         std::cout << video[row * width + col];
@@ -65,7 +69,7 @@ int main() {
       std::cout << '\n';
     }
     A+= 0.05; B += 0.05; C += 0.05;
-    std::this_thread::sleep_for(std::chrono::milliseconds(5));
+    std::this_thread::sleep_for(std::chrono::milliseconds(10)); // small delay so it isn't too fast
   }
   
 
